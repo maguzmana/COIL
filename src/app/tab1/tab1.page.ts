3f0625f0ed95e3130be844a815ee0c12f482606d
Vina del Mar <vina@duoc.acad>
2024-10-07 16:31:44 -0300

    Segundo commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  // Definir la propiedad formData como un objeto
+  formData: any = {
+    peso: '',
+    distribucionGrasa: '',
+    antecedentesFamiliares: '',
+    edad: '',
+    prediabetes: '',
+    alimentacion: ''
+  };
 
-  constructor() {}
+  private _storage: Storage | null = null;
 
+  // Constructor con el servicio de almacenamiento
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  // Inicializar el almacenamiento local
+  async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  // Método submitForm que se llamará al enviar el formulario
+  submitForm() {
+    console.log('Datos del formulario:', this.formData);
+    // Guardar los datos en el almacenamiento local
+    this._storage?.set('formData', this.formData);
+  }
 }
