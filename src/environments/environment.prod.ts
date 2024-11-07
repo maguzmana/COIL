0db865e2dfe08b0193799018df4da759438ea0ed
maguzmana <ma.guzmana@duocuc.cl>
2024-11-07 18:58:06 -0300

    Cambiando IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://35.174.155.239:5000'
+  apiUrl: 'http://44.203.176.142:5000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://35.174.155.239:5000'
+  apiUrl: 'http://44.203.176.142:5000'
 };
 
