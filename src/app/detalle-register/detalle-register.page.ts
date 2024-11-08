18af1587ddff34d5c8b9270fb75a0f9102591566
“PsTam27“ <pi.sat@duocuc.cl>
2024-11-08 19:59:20 -0300

    cambios varios inicio

---
+++ src/app/detalle-register/detalle-register.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-register',
+  templateUrl: './detalle-register.page.html',
+  styleUrls: ['./detalle-register.page.scss'],
+})
+export class DetalleRegisterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
