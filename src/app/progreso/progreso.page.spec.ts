05a9b3f14db17f2e60dc2366b5bd691d10e62bf0
“PsTam27“ <pi.sat@duocuc.cl>
2024-11-12 12:21:48 -0300

    cambios progreso

--- src/app/progreso/progreso.page.spec.ts
+++ src/app/progreso/progreso.page.spec.ts
@@ -1,11 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';  // Importa el módulo de Ionic
 import { ProgresoPage } from './progreso.page';
 
 describe('ProgresoPage', () => {
   let component: ProgresoPage;
   let fixture: ComponentFixture<ProgresoPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProgresoPage ],
+      imports: [ IonicModule.forRoot() ] // Asegúrate de importar el módulo Ionic
+    })
+    .compileComponents();  // Compila los componentes
+
     fixture = TestBed.createComponent(ProgresoPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -13,5 +20,11 @@ describe('ProgresoPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have selectedDate as a valid date string', () => {
+    // Verifica que la fecha seleccionada sea una cadena válida
+    expect(component.selectedDate).toBeTruthy();
+    expect(new Date(component.selectedDate).getTime()).not.toBeNaN();
   });
 });
