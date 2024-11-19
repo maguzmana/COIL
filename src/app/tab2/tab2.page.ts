7c2fd52ffe635d3c60d514e39558c41de269489c
PsTam27 <129803713+PsTam27@users.noreply.github.com>
2024-11-19 16:12:35 -0300

    Update tab2.page.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-/* tab2.page.ts */
+// tab2.page.ts
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
@@ -10,16 +10,30 @@ import { NavController } from '@ionic/angular';
 export class Tab2Page implements OnInit {
 
   userName: string = 'Nombre de usuario';
-  dailyTip: string = 'Consejo diario de salud';
-  dailyTip2: string = 'Consejo diario'
+  dailyTips: string[] = [
+    'Come más frutas. Son ricas en fibra y te ayudarán a mantenerte saludable por más tiempo.',
+    'Recuerda que si sientes alguna molestia de salud debes consultar a tu doctor de confianza.',
+    'Hidratarte adecuadamente es crucial para la salud de tus riñones.',
+    'Haz ejercicio regularmente para mantenerte en forma y mejorar tu bienestar general.',
+    'Reduce el consumo de sal para evitar problemas de hipertensión.'
+  ];
+  dailyTip: string = '';  // El consejo que se mostrará actualmente
+  currentTipIndex: number = 0;  // Índice para rotar los consejos
 
   constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
-    // Inicializamos los valores
-    this.userName = "Juan Carlos";  // Puedes cambiar este valor según corresponda
-    this.dailyTip = "Come más frutas. Son ricas en fibra y te ayudarán a mantenerte saludable por más tiempo."; // Ejemplo de consejo diario
-    this.dailyTip2 = "Recuerda que si sientes alguna molestia de salud debes consultar a tu doctor de confinza."; // Ejemplo de consejo diario
+    // Inicializamos el primer consejo
+    this.dailyTip = this.dailyTips[this.currentTipIndex];
+    this.startDailyTipRotation();  // Iniciar la rotación de los consejos
+  }
+
+  // Función para iniciar la rotación automática del consejo diario
+  startDailyTipRotation() {
+    setInterval(() => {
+      this.currentTipIndex = (this.currentTipIndex + 1) % this.dailyTips.length;
+      this.dailyTip = this.dailyTips[this.currentTipIndex];  // Actualizar el consejo
+    }, 5000);  // Cambia el consejo cada 5 segundos
   }
 
   irAChatBot() {
