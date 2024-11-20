116bd0a7e2f0555249bdac7226e7680c3dee349e
maguzmana <ma.guzmana@duocuc.cl>
2024-11-20 20:33:03 -0300

    Fotos en consejos

--- src/app/tips-enf-renal/tips-enf-renal.page.ts
+++ src/app/tips-enf-renal/tips-enf-renal.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tips-enf-renal',
@@ -6,23 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tips-enf-renal.page.scss'],
 })
 export class TipsEnfRenalPage {
-  isModalOpen = false; // Controla si el modal está abierto o cerrado
-  modalTitle = ''; // Título del modal
-  modalContent = ''; // Contenido dinámico del modal
-  
+  isModalOpen = false;
+  modalTitle = '';
+  modalContent = '';
+  modalImageUrl = '';
 
-  constructor() {}
-
-  //ngOnInit() {}
-
-  // Abre el modal con título y contenido dinámico
-  openModal(title: string, content: string) {
+  openModal(title: string, content: string, imageUrl: string) {
     this.modalTitle = title;
     this.modalContent = content;
-    this.isModalOpen = true; // Abre el modal
+    this.modalImageUrl = imageUrl;
+    this.isModalOpen = true;
   }
 
-  // Cierra el modal
   closeModal() {
     this.isModalOpen = false;
   }
--- src/app/tips-hipotiroidismo/tips-hipotiroidismo.page.ts
+++ src/app/tips-hipotiroidismo/tips-hipotiroidismo.page.ts
@@ -9,14 +9,16 @@ export class TipsHipotiroidismoPage {
   isModalOpen: boolean = false; // Controla si el modal está abierto o cerrado
   modalTitle: string = ''; // Título del modal
   modalContent: string = ''; // Contenido del modal
+  modalImageUrl: string = ''; // URL de la imagen del modal
 
   constructor() {}
 
   // Método para abrir el modal y pasarle el título y contenido
-  openModal(title: string, content: string) {
+  openModal(title: string, content: string, imageUrl: string) {
     this.modalTitle = title;
     this.modalContent = content;
-    this.isModalOpen = true; // Abre el modal
+    this.modalImageUrl = imageUrl;
+    this.isModalOpen = true;
   }
 
   // Método para cerrar el modal
