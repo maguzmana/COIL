3127b73bf1023d4822270e8ba23ac69eb425c778
maguzmana <ma.guzmana@duocuc.cl>
2024-11-20 05:10:22 -0300

    Imagenes 2

--- src/app/tips-hipotiroidismo/tips-hipotiroidismo.page.ts
+++ src/app/tips-hipotiroidismo/tips-hipotiroidismo.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tips-hipotiroidismo',
   templateUrl: './tips-hipotiroidismo.page.html',
   styleUrls: ['./tips-hipotiroidismo.page.scss'],
 })
-export class TipsHipotiroidismoPage implements OnInit {
+export class TipsHipotiroidismoPage {
+  isModalOpen: boolean = false; // Controla si el modal está abierto o cerrado
+  modalTitle: string = ''; // Título del modal
+  modalContent: string = ''; // Contenido del modal
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  // Método para abrir el modal y pasarle el título y contenido
+  openModal(title: string, content: string) {
+    this.modalTitle = title;
+    this.modalContent = content;
+    this.isModalOpen = true; // Abre el modal
   }
 
+  // Método para cerrar el modal
+  closeModal() {
+    this.isModalOpen = false; // Cierra el modal
+  }
 }
